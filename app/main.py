77098463b20e89f45c4bb90adc51c41ef4392da3
zain2323 <zainsiddiqui2323@gmail.com>
2024-09-14 22:52:35 +0500

    implemented ping

--- app/main.py
+++ app/main.py
@@ -7,9 +7,22 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    server_socket = socket.create_server(("127.0.0.1", 6379), reuse_port=True)
+    sock, addr = server_socket.accept() # wait for client
+    print(f'Connected with client at addr {addr}')
+    
+    while True:
+        request = sock.recv(1024)
+        request = request.decode("utf-8")
+        data = request.lower().strip().split('\n')[0]
+        if data == 'ping':
+            sock.send('+PONG\r\n'.encode('utf-8'))
+        
+        if data == "close":
+            sock.send("closed\n".encode("utf-8"))
+            break
+    sock.close()
+    server_socket.close()
 
 if __name__ == "__main__":
     main()
